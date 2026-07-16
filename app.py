1c531ad78b7d37d91a8309d79b50b502d73857bd
meindjo <entd2234@gmail.com>
2026-07-16 15:35:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py - AR24 (Flask template avec Login & Dashboard)
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
